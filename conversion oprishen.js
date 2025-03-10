6289b20fb3d242e5b506e25a6f6432188ff10e4c
your name <your-email@example.com>
2025-03-10 15:49:33 +0530

    Datatype conversion confusion

---
+++ conversion oprishen.js
@@ -0,0 +1,36 @@
+let score = true
+
+console.log(typeof score) // string
+console.log(typeof (score)) 
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+console.log( valueInNumber)  
+
+
+// "33"  => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true; 0=> false
+// "0" => true; "" => false
+// "name" => true;
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber)
